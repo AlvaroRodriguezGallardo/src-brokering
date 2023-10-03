3d7d3d47cbd1225d431f6d98ec5c394a877d3acd
AlvaroRodriguezGallardo <80212790+AlvaroRodriguezGallardo@users.noreply.github.com>
2023-10-03 13:05:17 +0200

    Update exercise_fn.py

--- example-fn/exercise_fn.py
+++ example-fn/exercise_fn.py
@@ -3,13 +3,10 @@ import numpy as np
 
 # The image is read, and it is in the same work directory
 image = cv2.imread('images/perro_husky.jpg',cv2.IMREAD_GRAYSCALE)
-print("aaaa")
+
+
 # Canny's algorithm implemented in OpenCV is used for borders detection
 face_borders = cv2.Canny(image,100,200)
 
-#cv2.imshow('Borders detection', face_borders)
 
 cv2.imwrite('images/bordes.jpg',face_borders)
-
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
