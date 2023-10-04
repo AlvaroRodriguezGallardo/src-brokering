74d6931cbd09faab8232a6437c6a7b944fa0fd07
Manuel Parra-Royón <manuparra@gmail.com>
2023-10-04 10:18:24 +0200

    New version - Review exercise_fn.py

--- example-fn/exercise_fn.py
+++ example-fn/exercise_fn.py
@@ -1,12 +1,16 @@
 import cv2
 import numpy as np
+import sys
+
+# Check FSOURCE variable to decide if the whole folder or just a single file.
+## Please refactor and review it.
 
 # The image is read, and it is in the same work directory
-image = cv2.imread('images/perro_husky.jpg',cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('images/'+sys.argv[1],cv2.IMREAD_GRAYSCALE)
 
 
 # Canny's algorithm implemented in OpenCV is used for borders detection
 face_borders = cv2.Canny(image,100,200)
 
 
-cv2.imwrite('images/bordes.jpg',face_borders)
+cv2.imwrite('images/edge_detection_' + str() + '.jpg',face_borders)
