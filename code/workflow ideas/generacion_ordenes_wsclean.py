9e1e6e52d7b9bfa06e12c326c55d4ee137f64828
Álvaro Rodríguez Gallardo <80212790+AlvaroRodriguezGallardo@users.noreply.github.com>
2024-03-18 13:16:03 +0100

    Add files via upload
    Starting with script that generates wsclean orders to execute

---
+++ code/workflow ideas/generacion_ordenes_wsclean.py
@@ -0,0 +1,48 @@
+
+
+# Constants that represents params we use for wsclean or other library
+
+# Param: Number of CPUs
+
+# Where we should find a .sif file
+BIND = "--bind"
+# Library we want to use. By default, LIBRARY="wsclean"
+LIBRARY="wsclean"
+# Param -mgain
+MGAIN = "-mgain"
+# Param -gain
+GAIN = "-gain"
+# Param -niter--> Number of iterations
+NITER = "-niter"
+# Param -log-time
+LOG_TIME = "-log-time"
+# Param -auto-mask
+AUTO_MASK = "-auto-mask"
+# Param -auto-threshold
+AUTO_THRESHOLD = "-auto-threshold"
+# Param -local-rms
+LOCAL_RMS = "-local-rms"
+# Param -channels-out
+CHANNELS_OUT = "-channels-out"
+# Param -join-channels
+JOIN_CHANNELS = "-join-channels"
+# Param -weight
+WEIGHT = "-weight"
+# Param briggs
+BRIGGS = "briggs"
+# Param -scale
+SCALE = "-scale"
+# Param -size--> (x,y)
+SIZE = "-size"
+# Param -multiscale
+MULTISCALE = "-multiscale"
+# Param -multiscale-scales--> list of numbers
+MULTISCALE_SCALES = "-multiscale-scales"
+# Param -multiscale-scale-bias
+MULTISCALE_SCALE_BIAS = "-multiscale-scale-bias"
+# Param -name--> Hera we have an image
+NAME = "-name"
+
+
+# INPUT: "python generacion_ordenes_wsclean.py <library> <.sif file> <image> <.ms file>. By default, library==wsclean "
+if __name__ == '__main__':
